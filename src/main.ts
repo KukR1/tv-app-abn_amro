7f34ca55f1613b8291b6a749aa80e15538c63694
KukR1 <nikdani012@gmail.com>
2026-08-21 15:18:47 +0200

    Revert

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,5 @@ import './style.css'
 import App from './App.vue'
 import router from './router'
 
-// Handle GitHub Pages 404 redirect
-const redirectPath = sessionStorage.getItem("redirectPath")
-if (redirectPath) {
-  sessionStorage.removeItem("redirectPath")
-  router.push(redirectPath)
-}
-
 createApp(App).use(router).mount('#app')
 
